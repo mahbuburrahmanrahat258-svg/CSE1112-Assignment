cb5102e0e4b82044e4d7f7e304d5890111442cb7
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-16 20:24:46 +0600

    Add files via upload

---
+++ Basics C Porgrams/Add two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+
+    printf ("Enter to number : ");
+    scanf ("%d %d",&num1,&num2);
+
+    sum=num1+num2;
+        printf ("Sum= %d\n",sum);
+    return 0;
+}
