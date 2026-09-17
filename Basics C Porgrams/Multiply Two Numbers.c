c9842660faaaedab012c1834d2a9a99dfb5ba264
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-17 22:00:36 +0600

    Add files via upload

---
+++ Basics C Porgrams/Multiply Two Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,multi;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1,&num2);
+
+    multi=num1*num2;
+    printf("Multi= %d",multi);
+    return 0;
+}
+
+
