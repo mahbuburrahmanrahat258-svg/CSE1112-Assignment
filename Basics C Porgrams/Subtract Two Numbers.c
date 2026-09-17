168363729cd6d62437a2008e9564e4b412523b0f
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-17 21:57:01 +0600

    Add files via upload

---
+++ Basics C Porgrams/Subtract Two Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,difference;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1,&num2);
+
+    difference=num1-num2;
+    printf("Difference= %d",difference);
+    return 0;
+}
+
