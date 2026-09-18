0cf326a4c917967a113a6e60ee75871ea8540b38
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-18 20:58:55 +0600

    Add files via upload

---
+++ Basics C Porgrams/Area of a Circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float r,area;
+
+    printf("Enter r: ");
+    scanf("%f",&r);
+
+    area=3.1416*r*r;
+
+    printf ("Area of circle= %.2f",area);
+
+    return 0;
+
+}
