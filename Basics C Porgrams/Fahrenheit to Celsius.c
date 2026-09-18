63da0897e92a848129b7f869ef810d02e35212ba
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-18 20:41:15 +0600

    Add files via upload

---
+++ Basics C Porgrams/Fahrenheit to Celsius.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float f,c;
+
+    printf("Enter temp. in f");
+    scanf("%f",&f);
+
+    c =(f-32)*5/9;
+
+    printf("Celsius = %.2f", c);
+
+    return 0;
+}
