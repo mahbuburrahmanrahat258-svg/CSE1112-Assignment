d7579f0c48f6bd9d316b81f589af15bb6b76c571
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-25 20:47:02 +0600

    Add files via upload

---
+++ DECISION-MAKING PROGRAMS/Student Passed or Failed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int marks;
+
+    printf("Enter marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 40)
+        printf("Passed");
+    else
+        printf("Failed");
+
+    return 0;
+}
